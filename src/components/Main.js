93269f76a187efc3b047284c530afc2573b7096c
Andrii Yarmola <andrii.yarmola@psd2html.com>
2016-12-14 16:40:51 +0200

    hame page

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,10 +5,6 @@ const Main = React.createClass({
 	render() {
 		return (
 			<div>
-				<h1>
-					<Link to="/">Hello and Welcome to My Single-Page App</Link>
-				</h1>
-				<img src={'images/img2.jpg'} alt="image description"/>
 				<div id="wrapper">
 					<div className="home-holder">
 						<div className="container">
@@ -18,26 +14,41 @@ const Main = React.createClass({
 									<button className="arrow a-top">
 										<i className="fa fa-angle-up" aria-hidden="true"></i>
 									</button>
-									<button className="arrow a-bottom">
-										<i className="fa fa-angle-down" aria-hidden="true"></i>
-									</button>
 									<button className="arrow a-left">
 										<i className="fa fa-angle-left" aria-hidden="true"></i>
 									</button>
 									<button className="arrow a-right">
 										<i className="fa fa-angle-right" aria-hidden="true"></i>
 									</button>
-									
 									<div className="cube">
 										<div className="face n1 medium">
-											<a href="#" class="link">
-												<span class="img-holder">
+											<Link to="/about" className="link">
+												<span className="img-holder">
+													<img src={'images/img4.jpg'} alt="image description"/>
 												</span>
-												<strong class="glitch" title="About me / contacts">About me / contacts</strong>
-											</a>
+												<strong className="glitch" data-title="About me / contacts">About me / contacts</strong>
+											</Link>
 										</div>
+										<div className="face n2">
+											<Link to="/commercy-list" className="link">
+												<span className="img-holder">
+													<img src={'images/img1.png'} alt="image description"/>
+												</span>
+												<strong className="glitch" data-title="Commercy works">Commercy works</strong>
+											</Link>
+										</div>
+										<div className="face n3"></div>
+										<div className="face n4"></div>
+										<div className="face n5">
+											<Link to="/react-list" className="link">
+												<span className="img-holder">
+													<img src={'images/img3.jpg'} alt="image description"/>
+												</span>
+												<strong className="glitch" data-title="React home tasks">React home tasks</strong>
+											</Link>
+										</div>
+										<div className="face n6"></div>
 									</div>
-									
 								</div>
 							</div>
 						</div>
